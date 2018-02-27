3b01b0f69699db92bc0a66b0ae94316ca0359a82
unknown <you@example.com>
2018-02-27 11:57:42 +0800

    脚手架

---
+++ src/component/App.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+  </main>
+</template>
+
+<script>
+    // 导出组件配置
+    export default {
+        data() {
+            return {
+                title: 'Hello World!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 30px;
+        color: red;
+    }
+</style>
